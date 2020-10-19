9655bb884babc3e013a9375ce83a023651049fa1
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2020-10-19 13:24:09 +0900

    URL 파라미터와 쿼리

--- router-tutorial/src/App.jsx
+++ router-tutorial/src/App.jsx
@@ -2,12 +2,29 @@ import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import About from './component/About';
 import Home from './component/Home';
+import Profile from './component/Profile';
 
 const App = () => {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to='/'>홈</Link>
+        </li>
+        <li>
+          <Link to='/about'>소개</Link>
+        </li>
+        <li>
+          <Link to='/profile/velopert'>velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to='/profile/gildong'>gildong 프로필</Link>
+        </li>
+      </ul>
+      <hr/>
       <Route path='/' component={Home} exact />
-      <Route path={['/about', '/info']} component={About} exact />
+      <Route path='/about' component={About} exact />
+      <Route path='/profile/:username' component={Profile} exact />
     </div>
   );
 };
