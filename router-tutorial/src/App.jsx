79c8e1af5c0049c5654560ba199e7009f464bafc
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2020-10-19 14:07:23 +0900

    Switch

--- router-tutorial/src/App.jsx
+++ router-tutorial/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 import About from './component/About';
 import HistorySample from './component/HistorySample';
 import Home from './component/Home';
@@ -23,10 +23,18 @@ const App = () => {
         </li>
       </ul>
       <hr/>
+      <Switch>
       <Route path='/' component={Home} exact />
       <Route path='/about' component={About} exact />
       <Route path='/profiles' component={Profiles} exact />
       <Route path='/history' component={HistorySample} exact />
+      <Route render={({location}) => (
+          <div>
+            <h2>이 페이는 존재하지 않습니다:</h2>
+            <p>{location.pathname}</p>
+          </div>
+      )} />
+      </Switch>
     </div>
   );
 };
