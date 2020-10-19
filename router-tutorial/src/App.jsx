4c53f627f7af7c8d1aaad4a6c7b82f376a0c4cf2
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2020-10-19 13:55:36 +0900

    리액트 라우터 부가 기능 history

--- router-tutorial/src/App.jsx
+++ router-tutorial/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import About from './component/About';
+import HistorySample from './component/HistorySample';
 import Home from './component/Home';
 import Profiles from './component/Profiles';
 
@@ -17,12 +18,15 @@ const App = () => {
         <li>
           <Link to='/profiles'>프로필</Link>
         </li>
-       
+        <li>
+          <Link to='/history'>History 예제</Link>
+        </li>
       </ul>
       <hr/>
       <Route path='/' component={Home} exact />
       <Route path='/about' component={About} exact />
       <Route path='/profiles' component={Profiles} exact />
+      <Route path='/history' component={HistorySample} exact />
     </div>
   );
 };
