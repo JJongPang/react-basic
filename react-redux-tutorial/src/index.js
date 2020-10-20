5591e9ef6bf0ca37b1fe5947cf23f1d8a4aea51e
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2020-10-20 15:38:16 +0900

    리덕스를 사용하여 리액트 애플리케이션 상태 관리하기

---
+++ react-redux-tutorial/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './containers/modules';
+
+
+//스토어 만들기
+const store = createStore(rootReducer, composeWithDevTools());
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider> 
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
