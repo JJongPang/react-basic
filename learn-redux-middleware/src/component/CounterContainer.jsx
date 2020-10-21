9c8c19ed33f00354f053024088824e7c040fe6d0
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2020-10-21 18:00:07 +0900

    미들웨어 파일 만들기

---
+++ learn-redux-middleware/src/component/CounterContainer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Counter from './Counter';
+import { increase, decrease } from '../module/counter';
+
+const CounterContainer = ({number, increase, decrease}) => {
+    return (
+        <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    );
+};
+
+export default connect(
+    state => ({
+        number: state.counter
+    }),
+    {
+        increase,
+        decrease
+    }
+)(CounterContainer);
